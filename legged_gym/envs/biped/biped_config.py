af68112d342f6700b0d981c505b537d2dfdf93d5
yuzy11 <yuzy@engineai.com.cn>
2024-11-20 12:59:25 +0800

    run ok

--- legged_gym/envs/biped/biped_config.py
+++ legged_gym/envs/biped/biped_config.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
@@ -30,87 +30,127 @@
 
 from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
-class BipedRoughCfg( LeggedRobotCfg ):
-    class env( LeggedRobotCfg.env):
+
+class BipedRoughCfg(LeggedRobotCfg):
+    class env(LeggedRobotCfg.env):
         num_envs = 4096
         num_observations = 169
         num_actions = 12
 
-    
-    class terrain( LeggedRobotCfg.terrain):
-        measured_points_x = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5] # 1mx1m rectangle (without center line)
-        measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
-        mesh_type='plane'
+    class terrain(LeggedRobotCfg.terrain):
+        # 1mx1m rectangle (without center line)
+        measured_points_x = [-0.5, -0.4, -0.3, -
+                             0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
+        measured_points_y = [-0.5, -0.4, -0.3, -
+                             0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
+        mesh_type = 'plane'
 
-    class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.90] # x,y,z [m]
-        default_joint_angles = { # = target angles [rad] when action = 0.0
+    class init_state(LeggedRobotCfg.init_state):
+        pos = [0.0, 0.0, 0.836]  # x,y,z [m]
+        default_joint_angles = {  # = target angles [rad] when action = 0.0
             'J01_HIP_ROLL_L': 0.0,
             'J02_HIP_YAW_L': 0.,
-            'J03_HIP_PITCH_L': -0.24,
-            'J04_KNEE_PITCH_L': 0.48,
-            'J05_ANKLE_PITCH_L': -0.24,
+            'J03_HIP_PITCH_L': -0.12,
+            'J04_KNEE_PITCH_L': 0.24,
+            'J05_ANKLE_PITCH_L': -0.12,
             'J06_ANKLE_ROLL_L': 0,
 
             'J07_HIP_ROLL_R': 0.0,
             'J08_HIP_YAW_R': 0.,
-            'J09_HIP_PITCH_R': -0.24,
-            'J10_KNEE_PITCH_R': 0.48,
-            'J11_ANKLE_PITCH_R': -0.24,
+            'J09_HIP_PITCH_R': -0.12,
+            'J10_KNEE_PITCH_R': 0.24,
+            'J11_ANKLE_PITCH_R': -0.12,
             'J12_ANKLE_ROLL_R': 0.
         }
 
-    class control( LeggedRobotCfg.control ):
+    class commands:
+        curriculum = False
+        max_curriculum = 1.
+        # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        num_commands = 4
+        resampling_time = 10.  # time before command are changed[s]
+        heading_command = False  # if true: compute ang vel command from heading error
+
+        class ranges:
+            lin_vel_x = [-1.0, 1.0]  # min max [m/s]
+            lin_vel_y = [-0.5, 0.5]   # min max [m/s]
+            ang_vel_yaw = [-1, 1]    # min max [rad/s]
+            heading = [-3.14, 3.14]
+
+    class control(LeggedRobotCfg.control):
+        control_type = 'P'  # P: position, V: velocity, T: torques
         # PD Drive parameters:
-        stiffness = {   'HIP_ROLL': 100.0, 'HIP_YAW': 100.0,
-                        'HIP_PITCH': 200., 'KNEE_PITCH': 200., 'ANKLE_PITCH': 40.,
-                        'ANKLE_ROLL': 40.}  # [N*m/rad]
-        damping = { 'HIP_ROLL': 3.0, 'HIP_YAW': 3.0,
-                    'HIP_PITCH': 6., 'KNEE_PITCH': 6., 'ANKLE_PITCH': 1.,
-                    'ANKLE_ROLL': 1.}  # [N*m*s/rad]     # [N*m*s/rad]
+        stiffness = {'HIP_ROLL': 160.0, 'HIP_YAW': 160.0,
+                     'HIP_PITCH': 160., 'KNEE_PITCH': 160., 'ANKLE_PITCH': 20.,
+                     'ANKLE_ROLL': 20.}  # [N*m/rad]
+        damping = {'HIP_ROLL': 6.0, 'HIP_YAW': 6.0,
+                   'HIP_PITCH': 6., 'KNEE_PITCH': 6., 'ANKLE_PITCH': 1.,
+                   'ANKLE_ROLL': 1.}  # [N*m*s/rad]     # [N*m*s/rad]
         # action scale: target angle = actionScale * action + defaultAngle
         action_scale = 0.25
         # decimation: Number of control action updates @ sim DT per policy DT
         decimation = 4
-        
-    class asset( LeggedRobotCfg.asset ):
-        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/biped/urdf/biped.urdf'
+
+    class asset(LeggedRobotCfg.asset):
+        # file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/biped/xml/biped.xml'
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/biped/urdf/sa01_rl.urdf'
         name = "biped"
-        foot_name = 'LINK_FOOT'
+        foot_name = 'LINK_ANKLE_ROLL'
         terminate_after_contacts_on = ['LINK_BASE']
         flip_visual_attachments = False
-        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
-  
-    class rewards( LeggedRobotCfg.rewards ):
+        self_collisions = 0  # 1 to disable, 0 to enable...bitwise filter
+        # fix_base_link = False  # fixe the base of the robot
+
+        # penalize_contacts_on = []
+        # terminate_after_contacts_on = []
+        # disable_gravity = False
+
+        # # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
+        # default_dof_drive_mode = 3
+        # self_collisions = 0  # 1 to disable, 0 to enable...bitwise filter
+        # # replace collision cylinders with capsules, leads to faster/more stable simulation
+        # replace_cylinder_with_capsule = True
+        # armature = 0.
+
+    class rewards(LeggedRobotCfg.rewards):
         soft_dof_pos_limit = 0.95
-        soft_dof_vel_limit = 0.9
-        soft_torque_limit = 0.9
-        max_contact_force = 300.
+        soft_dof_vel_limit = 0.8
+        soft_torque_limit = 0.8
+        max_contact_force = 350
         only_positive_rewards = False
-        base_height_target = 0.75
+        base_height_target = 0.83
+
         class scales:
             termination = -200.
             tracking_lin_vel = 1.0
-            tracking_ang_vel = 1.0
-            torques = -1.e-6
-            dof_acc = -2.e-8
-            lin_vel_z = -0.5
-            feet_air_time = 5.
-            dof_pos_limits = -1.
-            no_fly = 0.25
-            dof_vel = -0.0
-            ang_vel_xy = -0.0
-            feet_contact_forces = -0.
+            tracking_ang_vel = 0.5
 
-class BipedRoughCfgPPO( LeggedRobotCfgPPO ):
-    
-    class runner( LeggedRobotCfgPPO.runner ):
+            lin_vel_z = -0.5
+            base_height = -0.1
+            ang_vel_xy = -0.1
+            orientation = -1.0
+
+            feet_air_time = 5.
+            no_fly = 0.25
+            feet_contact_forces = -0.
+            collision = -1.
+
+            dof_pos_limits = -0
+
+            action_rate = -0.05
+            torques = -1.e-5
+            dof_vel = -0.0
+            dof_acc = -8.e-8
+
+            feet_stumble = -0.0
+            stand_still = -0.
+
+
+class BipedRoughCfgPPO(LeggedRobotCfgPPO):
+
+    class runner(LeggedRobotCfgPPO.runner):
         run_name = ''
         experiment_name = 'rough_biped'
 
-    class algorithm( LeggedRobotCfgPPO.algorithm):
+    class algorithm(LeggedRobotCfgPPO.algorithm):
         entropy_coef = 0.01
-
-
-
-  
---
+++ legged_gym/envs/biped/yuzy_test.py
@@ -0,0 +1,87 @@
+import math
+import numpy as np
+from isaacgym import gymapi
+from isaacgym import gymutil
+import os
+# Initialize gym
+gym = gymapi.acquire_gym()
+
+# create a simulation
+# configure common parameters
+sim_params = gymapi.SimParams()
+sim_params.dt = 1.0 / 60.0
+sim_params.substeps = 2
+sim_params.up_axis = gymapi.UP_AXIS_Z
+sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
+# set PhysX-specific parameters
+sim_params.physx.use_gpu = True
+sim_params.physx.solver_type = 1
+sim_params.physx.num_position_iterations = 6
+sim_params.physx.num_velocity_iterations = 1
+sim_params.physx.contact_offset = 0.01
+sim_params.physx.rest_offset = 0.0
+# Parse arguments
+args = gymutil.parse_arguments(description="yuzy11 test")
+sim = gym.create_sim(args.compute_device_id,
+                     args.graphics_device_id, args.physics_engine, sim_params)
+if sim is None:
+    print("*** Failed to create sim")
+    quit()
+
+# Creating a Ground Plane
+plane_params = gymapi.PlaneParams()
+plane_params.normal = gymapi.Vec3(0, 0, 1)  # z-up
+plane_params.distance = 0  # 距离
+plane_params.static_friction = 1  # 静摩擦系数
+plane_params.dynamic_friction = 1  # 动摩擦系数
+plane_params.restitution = 0
+gym.add_ground(sim, plane_params)
+
+# Loading Assets
+asset_root = os.path.dirname(__file__)+"/../../../resources/robots/biped"
+asset_file = "urdf/sa01_rl.urdf"
+asset_options = gymapi.AssetOptions()
+asset_options.fix_base_link = True
+# asset_options.flip_visual_attachments = True
+asset_options.armature = 0.01
+asset = gym.load_asset(sim, asset_root, asset_file, asset_options)
+
+# Environments and Actors
+num_envs = 4
+env_spacing = 1.0
+envs_per_row = 8
+env_lower = gymapi.Vec3(-env_spacing, -env_spacing, 0)
+env_upper = gymapi.Vec3(env_spacing, env_spacing, 0)
+
+envs = []
+actor_handles = []
+for i in range(num_envs):
+    env = gym.create_env(sim, env_lower, env_upper, envs_per_row)
+    envs.append(env)
+    pose = gymapi.Transform()
+    pose.p = gymapi.Vec3(0.0, 0.0, 1.0)
+    pose.r = gymapi.Quat.from_axis_angle(gymapi.Vec3(1, 0, 0), 0.0 * math.pi)
+    actor_handle = gym.create_actor(env, asset, pose, "MyActor", i, 1)
+    actor_handles.append(actor_handle)
+
+
+# Create viewer
+viewer = gym.create_viewer(sim, gymapi.CameraProperties())
+gym.subscribe_viewer_keyboard_event(viewer, gymapi.KEY_R, "reset")
+if viewer is None:
+    print("*** Failed to create viewer")
+    quit()
+# save initial state for reset
+initial_state = np.copy(gym.get_sim_rigid_body_states(sim, gymapi.STATE_ALL))
+while not gym.query_viewer_has_closed(viewer):
+    gym.simulate(sim)
+    gym.fetch_results(sim, True)
+    for evt in gym.query_viewer_action_events(viewer):
+        if evt.action == "reset" and evt.value > 0:
+            gym.set_sim_rigid_body_states(sim, initial_state, gymapi.STATE_ALL)
+    gym.step_graphics(sim)
+    gym.draw_viewer(viewer, sim, True)
+
+gym.destroy_viewer(viewer)
+gym.destroy_sim(sim)
+# Some common handles for later use
